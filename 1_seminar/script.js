54ab38aa16f1aae6286f5f62e20b363942fdf595
OlgaSuslova <alekssportmen@gmail.com>
2023-12-19 15:09:10 +0300

    Added files

---
+++ 1_seminar/script.js
@@ -0,0 +1,23 @@
+// Задание 6 (тайминг 15 минут)
+// 1. Создайте строку с вашем именем и строку с вашей фамилией.
+// Выведите эти данные в консоль
+// 2. Создайте переменную с текстом 'java' и переменную с текстом
+// 'script'. С помощью этих переменных и операции сложения строк
+// выведите в консоль строку 'javascript'.
+// 3. Создайте переменную с текстом 'hello' и переменную с текстом
+// 'world'. С помощью этих переменных и операции сложения строк
+// выведите в консоль строку 'hello world'
+
+
+
+const myMame = "Olga";
+const lastName = "Suslova";
+console.log(myMame, lastName);
+
+let java = "java";
+let script = "script";
+console.log(java + script);
+
+let hello = "hello";
+let world = "world";
+console.log(hello + world);
---
+++ 2_seminar/script.js
@@ -0,0 +1,125 @@
+// Текст задания
+// 1. Создать новый файл lesson2.html
+// 2. Создать переменную и спросить у пользователя “Ваш
+// возраст”
+// 3. Вывести на экран текст. Вам “возраст пользователя” лет
+
+// let userAge = prompt("Введите ваш возраст: ");
+// console.log(alert(`user age is ${userAge}`));
+
+// 4. Спросить у пользователя его имя
+// 5. Вывести на экран, Добро пожаловать на сайт “Имя
+// пользователя”
+
+// let userName = prompt("Input your name: ");
+// console.log(alert(`welcome to site ${userName}`));
+
+
+// Задание 2 (тайминг 15 минут)
+// Текст задания
+// 1. Найти остаток от деления двух переменных
+// a. let a = 13;
+// b. let b = 5;
+
+// let a = 3;
+// let b = 5 ;
+// console.log(a % b);
+
+// 2. Что выведет на экран (Сначала проговорите, потом проверьте)
+// let a;
+// b. alert(a);
+// 3. Что выведет на экран (Сначала проговорите, потом проверьте)
+// a. alert('abc' * 3);
+// 4. Что выведет на экран (Сначала проговорите, потом проверьте)
+// a. alert( 1 / 0);
+// b. alert(-1 / 0);
+// 5. Что выведет на экран (Сначала проговорите, потом проверьте)
+// a. alert('2' * '3');
+
+// Необходимо вывести в консоль результат суммы данных
+// переменных
+// a. let a = '2';
+// b. let b = '3';
+
+// let a = "2";
+// let b = "3";
+// console.log(a + b);
+// console.log(a * b);
+
+// Пользователь с клавиатуры вводит 2 числа, необходимо в консоль
+// вывести текст
+// a. Сумма чисел равна “результат”
+// b. Разность чисел равна “результат”
+// c. Произведение чисел равно “результат”
+// d. Частное чисел равно “результат”
+// e. Остаток от деления чисел равен “результат”
+
+// let firstNumber = Number(prompt("input first number: "));
+// let secondNumber = Number(prompt("input second number: "));
+// console.log(`summary is ${firstNumber + secondNumber}`);
+// console.log(`diff is ${firstNumber - secondNumber}`);
+// console.log(`product is ${firstNumber * secondNumber}`);
+// console.log(`division is ${firstNumber / secondNumber}`);
+// console.log(`remainder is ${firstNumber % secondNumber}`);
+
+
+// 1. Не запуская код, определите, что выведется в консоль:
+// a. console.log(String(true));
+// 2. Не запуская код, определите, что выведется в консоль:
+// a. console.log('a' + true);
+// 3. Не запуская код, определите, что выведется в консоль:
+// a. console.log(Number(true));
+// 4. Не запуская код, определите, что выведется в консоль:
+// a. console.log(true + 1);
+// 5. Не запуская код, определите, что выведется в консоль:
+// a. console.log(true + true)
+// 6. Не запуская код, определите, что выведется в консоль:
+// a. console.log(true - true)
+// 7. Не запуская код, определите, что выведется в консоль:
+// a. console.log( String(true) + Number(true) )
+
+
+
+// Задание 5 (тайминг 25 минут)
+// 1. Пользователь с клавиатуры вводит число, Необходимо создать
+// условный оператор который
+// a. Выводит в консоль “Число больше 5”
+// b. Выводит в консоль “Число меньше 5”
+// c. Выводит в консоль “Число равно 5”
+
+// let userNumber = Number(prompt('input number:'));
+// let variable = 5;
+// if (userNumber > variable) {
+//     console.log(`Number is more than ${variable}`);
+// } else if (userNumber < variable) {
+//     console.log(`Number is less than ${variable}`);
+// } else {
+//     console.log(`Number is ${variable}`);
+// }
+
+
+// 2. Даны переменные test1 и test2. Проверьте, равны ли их значения и
+// выведите соответствующее сообщение.
+
+// let userNumber1 = Number(prompt('input number 1:'));
+// let userNumber2 = Number(prompt('input number 2:'));
+// if (userNumber1 > userNumber2) {
+//     console.log(`${userNumber1} is more than ${userNumber2}`);
+// } else if (userNumber1 < userNumber2) {
+//     console.log(`${userNumber1} is less than ${userNumber2}`);
+// } else {
+//     console.log(`${userNumber1} is ${userNumber2}`);
+// }
+
+// 3. Пользовать с клавиатуры вводит 2 числа
+// a. Необходимо найти какое из двух чисел минимальное
+
+// 4. Пользователь с клавиатуры вводит число, Проверьте, что данная
+// переменная больше нуля и меньше 15.
+
+// let userNumber = Number(prompt('input number:'));
+// if (userNumber > 0 && userNumber < 15) {
+//     console.log("number is more than 0 and less than 15");
+// } else {
+//     console.log("number is failed verification");
+// }
